f518d1cbd65b6d11ae32d13b46a6f63680e89c06
Devin Wieland <dwieland@brotherhoodmutual.com>
2014-09-13 07:55:56 -0400

    formatted results, needs a set of 3

--- app/routes.rb
+++ app/routes.rb
@@ -12,6 +12,14 @@ get '/' do
 end
 
 post '/searchMovies' do
-  @movies = RottenMovie.find(:title => params[:title], :limit => 1)
+  @movies = RottenMovie.find(:title => params[:title], :limit => 3)
+  # binding.pry
   erb :index
 end
+
+# title       => @movie.title
+# year        => @movie.year
+# main actors => @movie.abridged_cast.each {|actor| puts actor.name }
+# rating      => @movie.ratings.critics_score
+# image       => @movie.posters.thumbnail (gives back a string url to the image)
+# synopsis (if there is one, which there is NOT always) => @movie.synopsis
