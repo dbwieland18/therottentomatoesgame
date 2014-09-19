49a623e48eba09b4728b3dd4fce5c42259bb0e1c
Devin Wieland <dwieland@brotherhoodmutual.com>
2014-09-19 07:07:00 -0400

    cycling though movies in game, recognizing the end. need to pick winner.

--- app/public/game.js
+++ app/public/game.js
@@ -1,3 +1,8 @@
+// extend base JS array class with super simple max function
+Array.max = function( array ){
+  return Math.max.apply( Math, array );
+};
+
 $(document).ready(function() {
   var movies = JSON.parse($('#id-holder').text());
   movies.shift();
@@ -14,6 +19,7 @@ $(document).ready(function() {
     var p1Score = parseInt($('#p1-score').val());
     var p2Score = parseInt($('#p2-score').val());
     var p3Score = parseInt($('#p3-score').val());
+    var scores = [p1Score,p2Score,p3Score]
 
     $.each([p1Guess, p2Guess, p3Guess], function(index, guess) {
       var num = index + 1
@@ -28,6 +34,7 @@ $(document).ready(function() {
         $('#p'+ num + '-score').html(current_score + (critics-guess))
       }
     });
+
     $('#critics-score').show();
     $('#submit-guesses').hide();
     $('#next-movie').show();
@@ -36,22 +43,28 @@ $(document).ready(function() {
   // showing/hiding buttons, loading next movie
   $('body').on('click', '#next-movie', function() {
     // console.log('moving on...')
+    if (movies.length == 0) {
+      $('#current-movie').append("<h1>We have a winner!</h1>")
+    }
+    else {
+      $('#submit-guesses').show();
+      $('#critics-score').hide();
+      $('#p1-guess').val("");
+      $('#p2-guess').val("");
+      $('#p3-guess').val("");
+      $.ajax({
+        url: "/getNextMovie",
+        type: "post",
+        data: {"next": movies}
+      }).done(function(data){
+        var nextMovie = $.parseJSON(data)
+        $('#current-movie img').attr('src', nextMovie[0].image)
+        $('#critics-score').attr('data-score', nextMovie[0].rating)
+        $('#critics-score').text(nextMovie[0].rating)
+        movies.shift();
+      });
+    }
     $('#next-movie').hide();
-    $('#submit-guesses').show();
-    $('#critics-score').hide();
-    $('#p1-guess').val("");
-    $('#p2-guess').val("");
-    $('#p3-guess').val("");
-    $.ajax({
-      url: "/getNextMovie",
-      type: "post",
-      data: {"next": movies}
-    }).done(function(data){
-      var nextMovie = $.parseJSON(data)
-      $('#current-movie img').attr('src', nextMovie[0].image)
-      $('#critics-score').attr('data-score', nextMovie[0].rating)
-      $('#critics-score').text(nextMovie[0].rating)
-    });
   });
 });
 
